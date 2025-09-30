d2a13755f7322e83624cbcc067c7cca82cb63cb9
EmreZiyaEfe <emreziyae@gmail.com>
2025-09-30 17:32:26 +0300

    sad

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -46,8 +46,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-//app.Urls.Add($"http://*:{port}");
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
